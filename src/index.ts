c3d8b2a865932ffc73497c99c9a63cf8c6204f79
Marcelo Zapaia <zapaiamarce@gmail.com>
2024-10-21 22:06:29 -0300

    creo que ahi va

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,23 @@
 import { initRouter } from "./routes";
 
-
 import { textContentInit } from "./components/text-content";
 import { botonesPptInit } from "./components/botones-ppt";
 import { botonAzulInit } from "./components/boton-azul";
 import { botonesPptConEventoInit } from "./components/botonConEvento";
 
+console.log("Cargo index.ts");
 
+(function main() {
+  console.log("Arrancó main");
+  const rootEl = document.querySelector("#root");
+  const posibleNullEl = document.createElement("div");
+  posibleNullEl.innerText = " No se cargo el rootEl";
 
+  initRouter(rootEl || posibleNullEl);
+  textContentInit();
+  botonesPptInit();
+  botonAzulInit();
+  botonesPptConEventoInit();
 
-
-(function main(){
-    
-    const rootEl = document.querySelector('#root');
-    const posibleNullEl = document.createElement('div');
-    posibleNullEl.innerText = " No se cargo el rootEl"
-
-    initRouter(rootEl || posibleNullEl )
-    textContentInit();
-    botonesPptInit();
-    botonAzulInit();
-    botonesPptConEventoInit();
-
+  console.log("Terminó main");
 })();
